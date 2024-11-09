d0b853d09656d15ae637404e90e42af9365a755c
TANGtongxue666 <2198557277@qq.com>
2024-11-09 20:23:28 +0800

    shuzhu_01.c

--- main.c
+++ main.c
@@ -1,9 +1,17 @@
-//
-//
-//
 #include <stdio.h>
-int main()
-{
-    printf("Hello World");
+
+int main() {
+    int arr[] = {1, 2, 3, 4, 5};
+
+    // 使用sizeof运算符计算数组长度
+    int length = sizeof(arr) / sizeof(arr[0]);
+
+    printf("数组的长度为: %d\n", length);
+
+    // 使用for循环输出数组元素
+    for (int i = 0; i < length; i++) {
+        printf("%d ", arr[i]);
+    }
+
     return 0;
 }
