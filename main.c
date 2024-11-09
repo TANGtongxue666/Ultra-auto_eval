13dc95a34ee4ef88419091972022dcb07072263a
TANGtongxue666 <2198557277@qq.com>
2024-11-09 20:14:00 +0800

    Update main.c

--- main.c
+++ main.c
@@ -1,9 +1,29 @@
-//
-//
-//
 #include <stdio.h>
-int main()
-{
-    printf("Hello World");
+#include <stdlib.h>
+
+#define ROWS 3
+#define COLS 3
+
+int main() {
+   
+    int matrix[ROWS][COLS];
+
+    
+    printf("请输入矩阵的元素（按行输入，每行%d个元素）：\n", COLS);
+    for (int i = 0; i < ROWS; i++) {
+        for (int j = 0; j < COLS; j++) {
+            scanf("%d", &matrix[i][j]);
+        }
+    }
+
+    ）
+    printf("输入的矩阵为：\n");
+    for (int i = 0; i < ROWS; i++) {
+        for (int j = 0; j < COLS; j++) {
+            printf("%d ", matrix[i][j]);
+        }
+        printf("\n");
+    }
+
     return 0;
 }
