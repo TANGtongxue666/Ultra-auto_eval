9423a77f7c02ff62cdc200bdaf09b4c6144a7011
TANGtongxue666 <2198557277@qq.com>
2024-11-09 20:08:30 +0800

    Update main.c

--- main.c
+++ main.c
@@ -3,7 +3,19 @@
 //
 #include <stdio.h>
 int main()
-{
-    printf("Hello World");
+{   int arr[] = {1, 2, 3, 4, 5};
+
+    
+    int length = sizeof(arr) / sizeof(arr[0]);
+
+    printf("数组的长度为: %d\n", length);
+
+    
+    for (int i = 0; i < length; i++) {
+        printf("%d ", arr[i]);
+    
+
+    
+    
     return 0;
 }
